933ab386c49690a1c6e786e402e0cc0d5f270c98
ZydrunasZujus <89936656+zydrunaszujus@users.noreply.github.com>
2021-09-06 22:18:05 +0300

    patobulinau uzduoti7,8 uzd

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("world!");
+            for (int i = 0; i < 100; i++)
+            {
+             Console.WriteLine("world!");
+            }
+           
         }
     }
 }
